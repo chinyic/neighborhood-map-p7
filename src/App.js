28d683ce30facd6daf2a61ff444b71de205d8edd
Chin Yi Chen <chinyic@uoregon.edu>
2018-09-18 20:56:34 -0700

    write loadScript function and insert API key

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 class App extends Component {
 
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCChKtHxb-Ay4LpdDllRMl3pT1kdel_rI8&callback=initMap")
+  }
+
   initMap = () => {
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
@@ -17,6 +21,20 @@ class App extends Component {
       </main>
     );
   }
+}
+/*
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+
+*/
+function loadScript(url){
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+
 }
 
 export default App;
