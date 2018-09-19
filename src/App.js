c1f817239cea3dfdf6fdc4b4588e7acefc321f3c
Chin Yi Chen <chinyic@uoregon.edu>
2018-09-18 21:02:45 -0700

    write window.initMap, and changed map css to 500px to view

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 
 class App extends Component {
 
+  componentDidMount(){
+    this.renderMap()
+  }
+
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCChKtHxb-Ay4LpdDllRMl3pT1kdel_rI8&callback=initMap")
+    window.initMap = this.initMap
+    // map inside window
   }
 
   initMap = () => {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
     });
