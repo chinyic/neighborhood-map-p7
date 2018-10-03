e5f39214aba13a757694f04f25b4a274e4d93d78
Chin Yi Chen <chinyic@uoregon.edu>
2018-10-02 21:31:10 -0700

    fixed errors in importing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ListView from './components/ListView';
 import './App.css';
 import axios from 'axios';
 import Map from './components/Map';
---
+++ src/components/ListView.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class ListView extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+        venues: []
+      }
+  }
+
+  render() {
+    return (
+        <main>
+
+        </main>
+      );
+    }
+  }
+
+
+export default ListView;
